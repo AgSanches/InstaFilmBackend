f3391f03bfae670966a13a36a3d7f368377ae87e
Adrian <adriangsanchez98@gmail.com>
2020-04-20 17:20:13 +0100

    Defined users model

---
+++ models/users.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = (sequelize, type) => {
+    return sequelize.define('users', {
+        name: {
+            type: type.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                isAlpha: true
+            }
+        },
+        lastname: {
+            type: type.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                isAlpha: true
+            }
+        },
+        email: {
+            type: type.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true,
+                isEmail: true
+            }
+        },
+        password: {
+            type: type.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [6,100]
+            }
+        },
+        rol: {
+            type: type.INTEGER,
+            allowNull: false,
+            defaultValue: 3, // 1 is Adm, 2 Teacher, 3 Student,
+            validate: {
+                notEmpty: true,
+                isIn: [1, 2, 3]
+            }
+        },
+        category: {
+            type: type.STRING,
+            allowNull: false
+        },
+        information: {
+            type: type.STRING,
+            validate: {
+                isAlpha: true
+            }
+        }
+    })
+}
+
+module.exports = User;
