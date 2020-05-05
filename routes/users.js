08f2dd312f9c1c4d483a9aa796a2dcdb6748f294
Adrian <adriangsanchez98@gmail.com>
2020-05-05 15:54:41 +0100

    Add middleware to endpoints.

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,11 @@ const jwtFunctions = require('../jwt')
 const router = express.Router();
 
 router.get('/users/:limit?', jwtFunctions.checkAuthenticated, UserController.getUsers);
-router.get('/user/:id', UserController.getUser);
+router.get('/user/:id', jwtFunctions.checkAuthenticated,UserController.getUser);
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
-router.put('/user/:id', UserController.updateUser);
-router.delete('/user/:id', UserController.deleteUser);
+router.put('/user/:id', jwtFunctions.checkAuthenticated, UserController.updateUser);
+router.delete('/user/:id', jwtFunctions.checkAuthenticated,UserController.deleteUser);
 
 
 module.exports = router;
