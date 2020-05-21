777dd9e36992ff67ec92fd91aef06d8687d1f1cc
Adrian <adriangsanchez98@gmail.com>
2020-05-21 15:36:34 +0100

    Added more endpoint

--- routes/landing.js
+++ routes/landing.js
@@ -4,7 +4,10 @@ const express = require('express');
 const router = express.Router();
 const landingController = require('../controllers/landing')
 
-router.get('/landing',  landingController.getLanding);
+router.get('/landing/movies/popular/:limit?',  landingController.getLandingMoviePopular);
+router.get('/landing/movies/latest/:limit?',  landingController.getLandingMovieLatest);
+router.get('/landing/series/popular/:limit?',  landingController.getLandingSeriePopular);
+router.get('/landing/series/latest/:limit?',  landingController.getLandingSerieLatest);
 
 
 module.exports = router;
