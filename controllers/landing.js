5647bb26055c2ca1634237752eb74670ea3e9d06
Adrian <adriangsanchez98@gmail.com>
2020-05-21 15:38:06 +0100

    Implementation of More popular movies, pending of movies valoration to change the sort method

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,11 +1,113 @@
 'use strict'
 
+const {Serie} = require('../db');
+const {Movie} = require('../db');
+
 const landingController = {
-    getLanding: (req, res) => {
-        return res.status(200).json({
-            hola: "hola"
-        })
-    }
+
+    getLandingMoviePopular: (req, res) => {
+
+        //TODO: When Movies have valoration, sort by them.
+        const options  = {
+            attributes: [
+                'id', 'title', 'releaseYear', 'image_path'
+            ],
+            order: [
+                ['createdAt', "DESC"]
+            ],
+            limit: 15
+        }
+
+        if (req.params.limit){
+            options.limit = req.params.limit;
+        }
+
+        Movie.findAll(options)
+            .then(movies => res.status(200).json(movies))
+            .catch(error => {
+
+                if(error.original.errno === 20) {
+                    return res.status(400).json({
+                        message: "El parámetro limit proporcionado no es válido, el valor debe ser un número."
+                    });
+                }
+                return res.status(500).json({
+                    message: "Ha ocurrido un problema, vuelva a intentarlo en otro momento."
+                })
+            })
+    },
+    getLandingMovieLatest: (req, res) => {
+
+        const landingContent = {}
+
+
+
+        Serie.findAll()
+            .then()
+            .catch(error => {
+
+                if(error.original.errno === 20) {
+                    return res.status(400).json({
+                        message: "El parámetro limit proporcionado no es válido, el valor debe ser un número."
+                    });
+                }
+                return res.status(500).json({
+                    message: "Ha ocurrido un problema, vuelva a intentarlo en otro momento."
+                })
+            })
+
+        console.log(landingContent);
+
+        return res.status(200).json(landingContent);
+    },
+    getLandingSeriePopular: (req, res) => {
+
+        const landingContent = {}
+
+
+
+        Serie.findAll()
+            .then()
+            .catch(error => {
+
+                if(error.original.errno === 20) {
+                    return res.status(400).json({
+                        message: "El parámetro limit proporcionado no es válido, el valor debe ser un número."
+                    });
+                }
+                return res.status(500).json({
+                    message: "Ha ocurrido un problema, vuelva a intentarlo en otro momento."
+                })
+            })
+
+        console.log(landingContent);
+
+        return res.status(200).json(landingContent);
+    },
+    getLandingSerieLatest: (req, res) => {
+
+        const landingContent = {}
+
+
+
+        Serie.findAll()
+            .then()
+            .catch(error => {
+
+                if(error.original.errno === 20) {
+                    return res.status(400).json({
+                        message: "El parámetro limit proporcionado no es válido, el valor debe ser un número."
+                    });
+                }
+                return res.status(500).json({
+                    message: "Ha ocurrido un problema, vuelva a intentarlo en otro momento."
+                })
+            })
+
+        console.log(landingContent);
+
+        return res.status(200).json(landingContent);
+    },
 }
 
 
