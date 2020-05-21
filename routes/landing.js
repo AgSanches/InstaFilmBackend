db60b4f80e8111a824fc9ea41047a8a008de3d58
Adrian <adriangsanchez98@gmail.com>
2020-05-21 16:41:49 +0100

    Refactor endpoint movies to films

--- routes/landing.js
+++ routes/landing.js
@@ -4,8 +4,8 @@ const express = require('express');
 const router = express.Router();
 const landingController = require('../controllers/landing')
 
-router.get('/landing/movies/popular/:limit?',  landingController.getLandingMoviePopular);
-router.get('/landing/movies/latest/:limit?',  landingController.getLandingMovieLatest);
+router.get('/landing/films/popular/:limit?',  landingController.getLandingMoviePopular);
+router.get('/landing/films/latest/:limit?',  landingController.getLandingMovieLatest);
 router.get('/landing/series/popular/:limit?',  landingController.getLandingSeriePopular);
 router.get('/landing/series/latest/:limit?',  landingController.getLandingSerieLatest);
 
