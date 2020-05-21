7da0e6e8da2dd28dda8596d75ffb720609319385
Adrian <adriangsanchez98@gmail.com>
2020-05-21 15:41:55 +0100

    Implementation of latest movies.

--- controllers/landing.js
+++ controllers/landing.js
@@ -38,12 +38,22 @@ const landingController = {
     },
     getLandingMovieLatest: (req, res) => {
 
-        const landingContent = {}
+        const options  = {
+            attributes: [
+                'id', 'title', 'releaseYear', 'image_path'
+            ],
+            order: [
+                ['createdAt', "DESC"]
+            ],
+            limit: 15
+        }
 
+        if (req.params.limit){
+            options.limit = req.params.limit;
+        }
 
-
-        Serie.findAll()
-            .then()
+        Movie.findAll(options)
+            .then(movies => res.status(200).json(movies))
             .catch(error => {
 
                 if(error.original.errno === 20) {
@@ -55,58 +65,18 @@ const landingController = {
                     message: "Ha ocurrido un problema, vuelva a intentarlo en otro momento."
                 })
             })
-
-        console.log(landingContent);
-
-        return res.status(200).json(landingContent);
     },
     getLandingSeriePopular: (req, res) => {
 
-        const landingContent = {}
-
-
-
-        Serie.findAll()
-            .then()
-            .catch(error => {
-
-                if(error.original.errno === 20) {
-                    return res.status(400).json({
-                        message: "El parámetro limit proporcionado no es válido, el valor debe ser un número."
-                    });
-                }
-                return res.status(500).json({
-                    message: "Ha ocurrido un problema, vuelva a intentarlo en otro momento."
-                })
-            })
-
-        console.log(landingContent);
-
-        return res.status(200).json(landingContent);
+        return res.status(200).json({
+            "status": "Not implemented"
+        });
     },
     getLandingSerieLatest: (req, res) => {
 
-        const landingContent = {}
-
-
-
-        Serie.findAll()
-            .then()
-            .catch(error => {
-
-                if(error.original.errno === 20) {
-                    return res.status(400).json({
-                        message: "El parámetro limit proporcionado no es válido, el valor debe ser un número."
-                    });
-                }
-                return res.status(500).json({
-                    message: "Ha ocurrido un problema, vuelva a intentarlo en otro momento."
-                })
-            })
-
-        console.log(landingContent);
-
-        return res.status(200).json(landingContent);
+        return res.status(200).json({
+            "status": "Not implemented"
+        });
     },
 }
 
