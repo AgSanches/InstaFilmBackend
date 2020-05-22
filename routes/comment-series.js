fb68639bad07b0a8a193dd1ffc60c419c072036d
Adrian <adriangsanchez98@gmail.com>
2020-05-22 17:08:26 +0100

    Add validation to comment creation.

--- routes/comment-series.js
+++ routes/comment-series.js
@@ -2,9 +2,18 @@
 
 const express = require('express');
 const router = express.Router();
+const jwtFunctions = require('../jwt')
+const { check } = require('express-validator');
+const checkErrors = require('./validation');
 const commentSeries = require('../controllers/comment-series');
 
-router.post('/series/create-comment',  commentSeries.createComment);
+router.post('/series/create-comment', [
+    jwtFunctions.checkAuthenticated,
+    check('content').not().isEmpty(),
+    check('seriesId').not().isEmpty(),
+    checkErrors,
+    jwtFunctions.setUserId
+], commentSeries.createComment);
 
 
 
